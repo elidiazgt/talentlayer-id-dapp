a2919a7175d190e68ce7e5bad791f60e0d1ff996
Michael Mueller <hello@michaelmueller.dev>
2022-07-22 16:16:02 +0200

    add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { HashRouter as Router } from 'react-router-dom';
 import { Web3ReactProvider } from '@web3-react/core';
 import { ExternalProvider, JsonRpcFetchFunc, Web3Provider } from '@ethersproject/providers';
 import { Box, Container, Typography } from '@mui/material';
@@ -12,27 +13,29 @@ function getLibrary(provider: ExternalProvider | JsonRpcFetchFunc): Web3Provider
 const App = () => {
   return (
     <Web3ReactProvider getLibrary={getLibrary}>
-      <Container maxWidth='xl'>
-        <Wallet />
-        <Typography variant='h4' component='h1' letterSpacing={0.5}>
-          TalentLayer
-        </Typography>
-        <Box
-          display='flex'
-          width='100%'
-          height='calc(100vh - 64px)'
-          alignItems='center'
-          justifyContent='center'
-          marginTop='auto'
-          marginBottom='auto'>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-          sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-          est laborum
-        </Box>
-      </Container>
+      <Router>
+        <Container maxWidth='xl'>
+          <Wallet />
+          <Typography variant='h4' component='h1' letterSpacing={0.5}>
+            TalentLayer
+          </Typography>
+          <Box
+            display='flex'
+            width='100%'
+            height='calc(100vh - 64px)'
+            alignItems='center'
+            justifyContent='center'
+            marginTop='auto'
+            marginBottom='auto'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+            mollit anim id est laborum
+          </Box>
+        </Container>
+      </Router>
     </Web3ReactProvider>
   );
 };
