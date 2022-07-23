a06b32f6da41d1110d88a8a8f7e84bed90c36870
Michael Mueller <hello@michaelmueller.dev>
2022-07-23 13:50:18 +0200

    add admin center

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,6 @@
+export { default as AdminCenterActions } from './AdminCenterActions';
 export { default as ChooseHandleForm } from './ChooseHandleForm';
 export { default as ConnectWallet } from './ConnectWallet';
 export { default as Header } from './Header';
+export { default as ReputationSummary } from './ReputationSummary';
 export { default as Wallet } from './Wallet';
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,5 @@
 export { default as Home } from './Home';
+export { default as AdminCenter } from './AdminCenter';
 export { default as ChooseHandle } from './ChooseHandle';
 export { default as MintHandle } from './MintHandle';
 export { default as MintHandleSuccess } from './MintHandleSuccess';
