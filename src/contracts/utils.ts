bd69c9c123f5832bf9d252a49d8a37e4cfecdd1d
Romain <rm@adversport.com>
2022-07-23 18:55:53 +0200

    Use new tlID mapping to changle for taken handle

--- src/contracts/utils.ts
+++ src/contracts/utils.ts
@@ -12,7 +12,6 @@ export const checkHandleUniqueness = async (
   }
   const signer = library.getSigner(account);
   const talentLayerID = new Contract(config.talentLayerIdAddress, TalentLayerIDABI.abi, signer);
-  const addressLinkedWithThisHandle = await talentLayerID.handles(newHandle);
-  const isHandleExist = addressLinkedWithThisHandle != ethers.constants.AddressZero;
-  return !isHandleExist;
+  const isHandleTaken = await talentLayerID.takenHandles(newHandle);
+  return !isHandleTaken;
 };
