f7c6d8121c239af425e6b8f881af8e6dceb5a997
Michael Mueller <hello@michaelmueller.dev>
2022-07-23 22:46:10 +0200

    fix merge conflict

--- src/components/ChooseHandleForm.tsx
+++ src/components/ChooseHandleForm.tsx
@@ -1,10 +1,7 @@
 import { useNavigate } from 'react-router-dom';
-import { useWeb3React } from '@web3-react/core';
-import { Web3Provider } from '@ethersproject/providers';
 import { Button, Stack, TextField } from '@mui/material';
 import { useFormik } from 'formik';
 import { useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { object, string } from 'yup';
 import TalentLayerContext from '../context/talentLayer';
 import { checkHandleUniqueness } from '../contracts/utils';
