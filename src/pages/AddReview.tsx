11ced6def4b5121e7ec18df561271eb6a1b91ed4
Romain <rm@adversport.com>
2022-07-23 23:09:49 +0200

    Complete review form

--- src/pages/AddReview.tsx
+++ src/pages/AddReview.tsx
@@ -1,7 +1,9 @@
 import { LoadingButton } from '@mui/lab';
-import { Alert, Box, Button, Card, CardContent, Grid, Typography } from '@mui/material';
+import { Alert, Box, Grid, Stack, TextField, Typography } from '@mui/material';
+import { useFormik } from 'formik';
 import { useContext, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { number, object, string } from 'yup';
 import TalentLayerContext from '../context/talentLayer';
 import { addReview } from '../contracts/utils';
 
@@ -11,11 +13,13 @@ const AddReview = () => {
   const [isMinting, setIsMinting] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
-  const handleAddReview = async () => {
+  const handleAddReview = async ({ review, rating }: { review: string; rating: number }) => {
     try {
       if (!jobId || !signer) return;
 
       setIsMinting(true);
+      // First post data to ipfs and get the CID
+      // Then add review
       await addReview(signer, jobId, 'tempURI');
     } catch (err) {
       // eslint-disable-next-line no-console
@@ -25,34 +29,77 @@ const AddReview = () => {
     }
   };
 
+  const formik = useFormik({
+    initialValues: {
+      review: '',
+      rating: 3,
+    },
+    enableReinitialize: true,
+    onSubmit: handleAddReview,
+    validationSchema: object({
+      review: string().required('review is required'),
+      rating: number().required('rating is required'),
+    }),
+  });
+  const { errors, isValid, touched, handleChange, handleSubmit, values, handleBlur } = formik;
+
   return (
     <Box display='flex' flexDirection='column' width='100%' alignItems='center' padding={10}>
       <Typography variant='h3' component='h1' letterSpacing={0.5} sx={{ p: 3 }} gutterBottom>
         AddReview
       </Typography>
-      <Typography sx={{ py: 2, mx: 3, fontSize: 20 }}>About Job:</Typography>
-      <Grid
-        container
-        direction='column'
-        justifyContent='center'
-        alignItems='center'
-        style={{ minHeight: '30vh' }}>
-        <Card sx={{ width: '30%', margin: '10px' }}>
-          <CardContent sx={{ textAlign: 'center' }}>FORM HERE</CardContent>
-          <LoadingButton
-            onClick={handleAddReview}
-            variant='contained'
-            size='large'
-            loading={isMinting}>
-            {isMinting ? 'Minting...' : 'Validate review'}
-          </LoadingButton>
-          {error && (
-            <Alert severity='error' sx={{ marginTop: '10px' }}>
-              {error}
-            </Alert>
-          )}
-        </Card>
-      </Grid>
+      <form onSubmit={handleSubmit}>
+        <Stack spacing={2} mt={2}>
+          <TextField
+            id='review'
+            name='review'
+            label='review'
+            value={values.review}
+            onChange={handleChange}
+            error={Boolean(errors.review && touched.review)}
+            helperText={errors.review && touched.review ? errors.review : ''}
+            onBlur={handleBlur}
+            multiline
+            rows={4}
+            required
+            style={{ width: 500 }}
+          />
+
+          <TextField
+            id='rating'
+            name='rating'
+            label='rating'
+            type='number'
+            InputLabelProps={{
+              shrink: true,
+            }}
+            value={values.rating}
+            onChange={handleChange}
+            error={Boolean(errors.rating && touched.rating)}
+            helperText={errors.rating && touched.rating ? errors.rating : ''}
+            onBlur={handleBlur}
+            inputProps={{ inputMode: 'numeric', min: 0, max: 5 }}
+            required
+            style={{ width: 500 }}
+          />
+
+          <Stack direction='row' justifyContent='center'>
+            <LoadingButton
+              variant='contained'
+              type='submit'
+              size='large'
+              disabled={!isValid || Object.keys(touched).length === 0}>
+              {isMinting ? 'Minting...' : 'Validate review'}
+            </LoadingButton>
+          </Stack>
+        </Stack>
+      </form>
+
+      {error && (
+        <Alert severity='error' sx={{ marginTop: '10px' }}>
+          {error}
+        </Alert>
+      )}
     </Box>
   );
 };
