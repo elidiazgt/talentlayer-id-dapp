55e6827ad327913a7cd333783ad157ec62ba4e62
Michael Mueller <hello@michaelmueller.dev>
2022-07-24 12:30:28 +0200

    add hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Web3ReactProvider } from '@web3-react/core';
 import { Container } from '@mui/material';
 import { TalentLayerProvider } from './context/talentLayer';
