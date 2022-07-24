1f36246b44e4b9baf1ea1763061bf687c99402be
Romain <rm@adversport.com>
2022-07-24 12:12:31 +0200

    use svg logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,12 +6,7 @@ const Header = () => {
   return (
     <Box p={5} display='flex' width='100%' alignItems='center' justifyContent='space-between'>
       <Link to='/' style={{ textDecoration: 'none', color: 'inherit' }}>
-        <CardMedia
-          component='img'
-          height='40'
-          image='/talent-layer-light-40.png'
-          title='TalentLayer logo'
-        />
+        <CardMedia component='img' height='35' image='/logo.svg' title='TalentLayer logo' />
       </Link>
       <Wallet />
     </Box>
