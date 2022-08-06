e47867fdc418065e1263d778824eb5d727b67a2f
Elidiazgt <elidiaz.gt@gmail.com>
2022-08-06 00:22:45 -0600

    add new address textfield

--- src/components/RecoverComponent.tsx
+++ src/components/RecoverComponent.tsx
@@ -6,7 +6,26 @@ import { useContext } from 'react';
 const RecoverComponent = () => {
 
 
-    return true
+    return <div>
+        <h1>Recover my Id</h1>
+        <form >
+        <Stack spacing={2} mt={2}>
+            <TextField
+
+                label='Write down your new Address'
+            />
+            <Stack direction='row' justifyContent='center'>
+                <Button
+                    variant='outlined'
+                    type='submit'
+                    size='large'
+                >
+                    Transfer My Id
+                </Button>
+            </Stack>
+        </Stack>
+        </form>
+    </div>
 }
 
 
