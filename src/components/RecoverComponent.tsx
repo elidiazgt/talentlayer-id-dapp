983b2280bc6ef28128b4a6a8f5223d284ca748d2
Elidiazgt <elidiaz.gt@gmail.com>
2022-08-25 00:27:18 -0600

    Join common components and add form event

--- src/components/RecoverComponent.tsx
+++ src/components/RecoverComponent.tsx
@@ -1,38 +1,75 @@
 import { Button, Stack, TextField } from '@mui/material';
-import { useFormik } from 'formik';
-
+import { AnyAaaaRecord } from 'dns';
+import { useState, useContext } from 'react';
+import RecoverComponentResult from '../components/RecoverComponentResult';
+import TalentLayerContext from '../context/talentLayer';
 const RecoverComponent = () => {
+    const { talentLayerId, talentLayerHandle } = useContext(TalentLayerContext);
+    const [showform, setShowform] = useState(false)
+    const [state, setState] = useState(
+        { id: '', wallet: '', password: '' }
+    );
+    console.log('talentLayerHandle', talentLayerHandle)
     let wallet = 'xxxxx'
-
+    function handleChange(e: Event) {
+        const value = e.target.value;
+        setState({ 
+            ...state,
+            [e.target.name]: value
+        });
+    }
+    let handleLoginClick = () => {
+        setShowform(true)
+    }
+    let recover = () => {
+        console.log('recover', state)
+    }
     return <div>
         <h1>Recover my Id</h1>
-        <form >
+        {showform ?
             <Stack spacing={2} mt={2}>
-                <TextField label='Your TalentLayer ID Handle'/>
-                <TextField label='Your Original Wallet'/>
-                <TextField label='Your Emergency Recovery Password' />
+                <TextField name='id' label='Your TalentLayer ID Handle' onChange={handleChange} />
+                <TextField name='wallet' label='Your Original Wallet' onChange={handleChange} />
+                <TextField name='password' label='Your Emergency Recovery Password' onChange={handleChange} />
                 <div>
                     Your TalentLayer ID will be transfered to the wallet you currently have connected upon initiation.
                     Does this look correct?
                     [{wallet}]
-
                 </div>
                 <Stack direction='row' justifyContent='center'>
                     <Button
                         variant='outlined'
                         type='submit'
                         size='large'
+                        onClick={recover}
                     >
                         Initiate Recovery
                     </Button>
                 </Stack>
             </Stack>
-        </form>
+            :
+            <Stack spacing={2} mt={2}>
+                <div>
+                    If you have lost access to the wallet holding your TalentLayer ID,
+                    You have the option to initiate an emergency Recovery
+                    In order to initiate a recovery, you must have your recovery
+                    password that was provided to you when you initiated your account.
+                </div>
+                <Stack direction='row' justifyContent='center'>
+                    <Button
+                        variant='outlined'
+                        size='large'
+                        onClick={handleLoginClick}
+                    >
+                        Recover My Account
+                    </Button>
+                </Stack>
+                <div>
+                    Currently this feature is only available for wallets
+                    that have Proof of Humanity verified.
+                </div>
+            </Stack>
+        }
     </div>
-    
 }
-
-
-
-
 export default RecoverComponent;
--- src/pages/RecoverId.tsx
+++ src/pages/RecoverId.tsx
@@ -1,43 +1,23 @@
 // import { useParams } from 'react-router-dom';
 import { Box, Grid } from '@mui/material';
 import { useContext } from 'react';
-import TalentLayerContext from '../context/talentLayer';
-
 import { RecoverComponent } from '../components';
-import { RecoverComponentNotice } from '../components';
-import { RecoverComponentResult } from '../components';
-
-
-
 const RecoverId = () => {
-  const { talentLayerId, talentLayerHandle } = useContext(TalentLayerContext);
-
-  // const { handle } = useParams<{ handle?: string }>();
-  const isConnected = talentLayerId && talentLayerId !== '0';
-  let steps = 0;
   return (
     <Box display='flex' justifyContent='flex-start' alignItems='center' width='100%'>
-
       <Box alignItems='center' sx={{ width: '100%' }}>
-
       <Grid
         container
         direction='row'
         justifyContent='center'
         alignItems='center'
         style={{ minHeight: '30vh' }}>
-        {isConnected ? (
-          <div>
-            <RecoverComponent />
-          {/* <RecoverComponentNotice />
-          <RecoverComponentResult /> */}
+          <div style={{border: '1px color red'}} >
+           <RecoverComponent />
           </div>
-    
-      ):(<div></div>)}
       </Grid>
-      </Box>
+      </Box> 
     </Box>
   );
 };
-
 export default RecoverId;
